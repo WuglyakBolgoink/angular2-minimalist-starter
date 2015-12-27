db7a4a12be45cefbda8d8e57782d5c0cb49794a1
Roger Padilla <rpc@Rogers-MacBook-Pro.local>
2015-12-26 20:09:58 -0500

    Removed deprecated directive

--- client/components/home/home.component.ts
+++ client/components/home/home.component.ts
@@ -1,8 +1,6 @@
 import {Component} from 'angular2/core';
-import {XLarge} from '../../directives/XLarge';
 
 @Component({
-  directives: [XLarge],
   selector: 'home',
   templateUrl: './components/home/home.component.html'
 })
