1b9a375cf792d964ad8778a6917a1807813d753c
Roger Padilla <rogerpadilla@Rogers-MacBook-Pro.local>
2016-07-19 16:37:10 -0500

    Added routing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , ChangeDetectionStrategy } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import '../assets/css/styles.css';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  directives: [ROUTER_DIRECTIVES],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent { }
+export class AppComponent {
+
+  title = 'Hello from Angular 2 minimalist starter';
+
+}
