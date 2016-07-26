4e8a2963f2cd5471e3d1e5f2a7feb7fc9c7e5e9f
Roger Padilla <rogerpadilla@Rogers-MacBook-Pro.local>
2016-07-25 22:40:39 -0500

    Used ViewEncapsulation.None on AboutComponent also

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-about',
   templateUrl: './about.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutComponent {
 
