66fc6b31fdb28b184b73a27325b14cbb44f6c0c4
Roger Padilla <rogerpadilla@Rogers-MacBook-Pro.local>
2016-07-26 12:50:34 -0500

    Improved sourcemaps generation

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,7 +4,21 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'source-map',
+
+  /**
+   * Switch loaders to debug mode.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#debug
+   */
+  debug: true,
+
+  /**
+   * Developer tool to enhance debugging
+   *
+   * See: http://webpack.github.io/docs/configuration.html#devtool
+   * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+   */
+  devtool: 'cheap-module-source-map',
 
   output: {
     path: helpers.root('dist'),
